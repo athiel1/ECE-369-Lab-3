96803a99f2b7fc45705279c7b7556d1c62cec678
athiel1 <157844495+athiel1@users.noreply.github.com>
2024-09-16 18:13:58 -0700

    Update vbsme.py

---
+++ vbsme.py
@@ -0,0 +1,8 @@
+''' read in i, j, k, l. The variables i and j are the frame dimensions frame[i][j], and k and l are the window dimensions window[k][l]
+The variables x and y are the top left coordinate of the window. x and y are initialized to zero because we need to start at 
+top left corner of the frame, (0, 0).
+
+for x = 0; x <= i - k; x++
+
+
+
